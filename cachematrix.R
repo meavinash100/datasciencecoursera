e6beaf1e61c290d3c1553195316b81633d50c63a
meavinash100 <meavinash100@gmail.com>
2015-10-07 17:03:07 +0530

    R programming Matrix Inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,15 @@
 # This function will create a matrix and calculates inverse of the matrix as well 
 # cache the value of matrix inversion
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+      mat_inverse <- NULL
+      set <- function(y) {
+      x <<- y
+      mat_inverse <<- NULL
+      }
+      get <- function() x
+      set_inverse <- function(inverse) mat_inverse <<- inverse
+      get_inverse <- function() mat_inverse
+      list(set=set, get=get, set_inverse=set_inverse, get_inverse=get_inverse)
 }
 
 
@@ -19,13 +19,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 # This function assumes that the matrix is always invertible.
 cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data.")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data)
-  x$setinverse(inv)
-  inv
+      mat_inverse <- x$getinverse()
+      if(!is.null(inv)) {
+          message("getting cached data.")
+          return(mat_inverse)
+      }
+      data <- x$get()
+      mat_inverse <- solve(data)
+      x$set_inverse(mat_inverse)
+      mat_inverse
 }
