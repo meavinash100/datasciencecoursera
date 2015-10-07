919164a6938740ac2eaeec4f174682a28e78c02c
meavinash100 <meavinash100@gmail.com>
2015-10-07 16:54:56 +0530

    R programming Matrix Inversion

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+# This function will create a matrix and calculates inverse of the matrix as well 
+# cache the value of matrix inversion
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+
+# The following function returns the inverse of the matrix. It first checks if invert
+#is available in cache if so retrieves that else calculates inverse
+
+
+# This function assumes that the matrix is always invertible.
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
+}
